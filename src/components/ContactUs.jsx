d067220845b4353e0b789982a80659c408407d82
balramswainn <balramswain789@gmail.com>
2026-05-22 14:55:18 +0530

    created form

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
 import Title from './Title'
 import assets from '../assets/assets'
+import toast from 'react-hot-toast';
 
 const ContactUs = () => {
+
+   const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "8d537e1d-7aad-4d7f-b78a-df9d53c26c95");
+
+    try{
+          const response = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+          toast.success("Thank you for your submission!")
+          event.target.reset();
+        } else {
+          // console.log("Error", data);
+          toast.error(data.message);
+        }
+      }catch (error){
+        toast.error(error.message);
+    }
+    
+  };
+
   return (
     <div id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
       <Title title='Reach out to us' desc="From strategy to execution, we craft digital solutions that move your business forward." />
 
-      <form className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full' >
+      <form onSubmit={onSubmit} className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full' >
         <div>
           <p className='mb-2 text-sm font-medium'>Your name</p>
           <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
               <img src={assets.person_icon} alt="" />
-              <input type="text" placeholder='Enter your name' className='w-full p-3 text-sm outline-none' required/>
+              <input name="name" type="text" placeholder='Enter your name' className='w-full p-3 text-sm outline-none' required/>
           </div>
         </div>
 
@@ -20,13 +49,13 @@ const ContactUs = () => {
           <p className='mb-2 text-sm font-medium'>Email id</p>
           <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
               <img src={assets.email_icon} alt="" />
-              <input type="text" placeholder='Enter your email' className='w-full p-3 text-sm outline-none' required/>
+              <input name="email" type="text" placeholder='Enter your email' className='w-full p-3 text-sm outline-none' required/>
           </div>
         </div>
 
         <div className='sm:col-span-2'>
           <p className='mb-2 text-sm font-medium'>Message</p>
-          <textarea rows={8} placeholder='Enter your message' className='w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600'></textarea>
+          <textarea name="message" rows={8} placeholder='Enter your message' className='w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600' required />
         </div>
 
         <button type="submit" className='w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 rounded-full cursor-pointer hover:scale-103 transition-all'>
@@ -38,3 +67,37 @@ const ContactUs = () => {
 }
 
 export default ContactUs
+
+
+
+
+// 8d537e1d-7aad-4d7f-b78a-df9d53c26c95
+// import { useState } from 'react';
+
+// export default function ContactForm() {
+//   const [result, setResult] = useState("");
+
+//   const onSubmit = async (event) => {
+//     event.preventDefault();
+//     const formData = new FormData(event.target);
+//     formData.append("access_key", "8d537e1d-7aad-4d7f-b78a-df9d53c26c95");
+
+//     const response = await fetch("https://api.web3forms.com/submit", {
+//       method: "POST",
+//       body: formData
+//     });
+
+//     const data = await response.json();
+//     setResult(data.success ? "Success!" : "Error");
+//   };
+
+//   return (
+//     <form onSubmit={onSubmit}>
+//       <input type="text" name="name" required/>
+//       <input type="email" name="email" required/>
+//       <textarea name="message" required></textarea>
+//       <button type="submit">Submit</button>
+//       <p>{result}</p>
+//     </form>
+//   );
+// }
