d30d03f4fc8e0fdf6ba202581f827ff45a93e231
balramswainn <balramswain789@gmail.com>
2026-05-22 18:44:44 +0530

    footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero';
 import TrustedBy from './components/TrustedBy';
@@ -7,9 +7,37 @@ import OurWork from './components/OurWork';
 import Teams from './components/Teams';
 import ContactUs from './components/ContactUs';
 import { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer';
 
 const App = () => {
+
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
+  
+  const dotRef = useRef(null);
+  const outlineRef = useRef(null);
+
+  //Refs for custom cursor Position tracking
+  const mouse = useRef({x:0, y:0})
+  const position = useRef({x:0, y:0})
+
+  useEffect(()=>{
+    const handleMouseMove =(e)=>{
+      mouse.current.x = e.clientX
+      mouse.current.y = e.clientY
+    }
+
+    document.addEventListener('mousemove',handleMouseMove)
+
+    // const animate = () = {
+    //   position.current.x += (mouse.current.x - position.current.x) * 0.1 
+    //   position.current.xy+= (mouse.current.y - position.current.y) * 0.1 
+
+    //   if(dotRef.current && outlineRef.current){
+    //     dotRef.current.style.transform
+    //   }
+    // }
+  },[])
+
   return (
     <div className='dark:bg-black relative'>
       <Toaster />
@@ -20,6 +48,13 @@ const App = () => {
       <OurWork />
       <Teams />
       <ContactUs />
+      <Footer theme={theme}/>
+
+      {/* Custom Cursor Ring */}
+      <div ref={outlineRef} className='fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]'></div>
+
+      {/* Custom Cursor Dot */}
+      <div ref={dotRef} className='fixed top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]'></div>
     </div>
   )
 }
