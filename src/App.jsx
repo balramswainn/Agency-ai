bdf45efa0365014e1d5558bf733b680e88fd156f
balramswainn <balramswain789@gmail.com>
2026-05-23 15:41:01 +0530

    added animation to all components

--- src/App.jsx
+++ src/App.jsx
@@ -28,14 +28,22 @@ const App = () => {
 
     document.addEventListener('mousemove',handleMouseMove)
 
-    // const animate = () = {
-    //   position.current.x += (mouse.current.x - position.current.x) * 0.1 
-    //   position.current.xy+= (mouse.current.y - position.current.y) * 0.1 
+    const animate = () => {
+      position.current.x += (mouse.current.x - position.current.x) * 0.1 
+      position.current.y += (mouse.current.y - position.current.y) * 0.1 
 
-    //   if(dotRef.current && outlineRef.current){
-    //     dotRef.current.style.transform
-    //   }
-    // }
+      if(dotRef.current && outlineRef.current){
+        dotRef.current.style.transform = `translate3d(${mouse.current.x - 6}px, ${mouse.current.y - 6}px, 0)`
+        outlineRef.current.style.transform = `translate3d(${position.current.x - 20}px, ${mouse.current.y - 20}px, 0)`
+      }
+      requestAnimationFrame(animate)
+    }
+
+    animate()
+
+    return ()=>{
+      document.removeEventListener('mousemove', handleMouseMove)
+    }
   },[])
 
   return (
@@ -51,7 +59,8 @@ const App = () => {
       <Footer theme={theme}/>
 
       {/* Custom Cursor Ring */}
-      <div ref={outlineRef} className='fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]'></div>
+      <div ref={outlineRef} className='fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]' 
+      style={{transition: 'transform 0.1s ease-out'}}></div>
 
       {/* Custom Cursor Dot */}
       <div ref={dotRef} className='fixed top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]'></div>
